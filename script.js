b0540d2d17642f1a1ed516a0660691b1dcdeec69
notshriyansh <shriyansh707@gmaiil.com>
2025-10-17 23:55:47 +0530

    JS code for script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let darkBtn = document.getElementById("darkModeBtn");
+let body = document.body;
+
+darkBtn.addEventListener("click", function(){
+    body.classList.toggle("dark-mode");
+});
+
+let increaseBtn = document.getElementById("increaseFont")
+let decreaseBtn = document.getElementById("decreaseFont")
+
+let progressBar = document.getElementById("progressBar");
+
+let currentSize = 16;
+
+increaseBtn.addEventListener("click", function(){
+    currentSize += 2;
+    body.style.fontSize = currentSize + "px";
+});
+
+decreaseBtn.addEventListener("click", function(){
+    currentSize -= 2;
+    body.style.fontSize = currentSize + "px";
+} );
+
+window.addEventListener("scroll", function(){
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    let progress = (window.srollY/totalHeight) *100;
+
+    progressBar.style.width = progress + "%";
+});
